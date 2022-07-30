34453689a7f168282dec4a936cc457027b6d392a
AndreyKapus <kapustnikov@ukr.net>
2022-07-30 11:57:10 +0300

    first task

---
+++ src/js/01-gallery.js
@@ -0,0 +1,29 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// Change code below this line
+const galleryRef = document.querySelector('.gallery');
+const galleryArray = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li><a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a></li>`;
+  })
+  .join(' ');
+
+galleryRef.insertAdjacentHTML('beforeend', galleryArray);
+
+galleryRef.addEventListener('click', onCardClick);
+
+function onCardClick(e) {
+  e.preventDefault();
+  if (!e.target.classList.contains('gallery__image')) {
+    return;
+  }
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
